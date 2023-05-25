8bc3e81af355301f3724a41072a1377638bc3f64
Will <wrightw404@gmail.com>
2023-05-25 03:07:31 -0400

    refactored classes

--- pages/projects.js
+++ pages/projects.js
@@ -87,11 +87,11 @@ export default function Projects() {
 
 
 
-    <div class="container my-12 mx-auto px-4 md:px-12">
-    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+    <div className="container my-12 mx-auto px-4 md:px-12">
+    <div className="flex flex-wrap -mx-1 lg:-mx-4">
 
    
-        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article className="overflow-hidden rounded-lg shadow-lg">
                 <a target="blank" href="https://shorethingmarket.vercel.app/">
                     <Image alt="Placeholder" className="block h-auto w-full hover:scale-75" src={stm}></Image>
